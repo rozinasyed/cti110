1ba59d1c0b5e9befb5e7c4e04b776b4311e5db42
rozinasyed <syedr2547@student.faytechcc.edu>
2017-06-06 20:03:25 -0600

    Add files via upload

---
+++ M2T1_SalesPrediction_FirstLast.py
@@ -0,0 +1,10 @@
+# A brief description of the project
+# Date
+# CTI-110 M2T1 - Sales Prediction
+# Rozina
+# Get the projected total sales.
+projectedTotalSales = float( input("Enter the projected amount" + \
+                                   "of total sales: " ))
+profit =  0.23 * projectedTotalSales 
+
+print( "The profit is $" + format( profit , ",.2f" ))
